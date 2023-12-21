05e0cf0b1b244a08b9871278703bd7389655af81
HackerMan237 <93648451+LekeneCedric@users.noreply.github.com>
2023-12-21 20:15:55 +0100

    add makefile

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,14 @@
-from models.file_item import file_item
-from enums.files_type import file_type
-from actions.files_operations import list_dir, load_file, read_root
+import sys
 from os import path
+
+sys.path.append('models/')
+sys.path.append('enums/')
+sys.path.append('actions/')
+
+from file_item import file_item
+from files_type import file_type
+from files_operations import list_dir, load_file, read_root
+
 
 ROOT_PATH = '/home/code237/Videos/MANGA'
 if __name__ == '__main__':
@@ -11,7 +18,7 @@ if __name__ == '__main__':
   root = file_item('root', file_type.DIR, ROOT_PATH,[])
 
   for file in files:
-    
+
       file = path.join(ROOT_PATH, file)
       is_dir = path.isdir(file)
       type = file_type.DIR if is_dir else file_type.FILE
